e208c662e156bbc23865a74f998e27e7927badf0
Somasundaram Alapati <soma.alapati@deque.com>
2022-05-13 14:28:36 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # urls-to-screenshots
 Generate full page screenshots by taking list of URLs as input
+
+# Prerequisites:
+- Java
+- Maven
+- TestNG (install it in Eclipse if you run tests from Eclipse)
+
+# Steps to Configure:
+1. Add the URLs in a .xlsx file.
+2. Give names to the excel file tab and the file.
+3. Place this .xlsx file in sr/main/resources filder.
+4. Open the URLtoScreenshot.java file > go to line 107 > Add file name and tab name as parameter values (2nd & 3rd parameters).
+
+# Results:
+- You can get the created screenshots in target/screenshots folder.
+- Invalid screenshots (whose length is more than 5000px) can be found in target/skipped_screenshots.
+
+# Parameter - Index:
+- In "URLtoScreenshot.Java" file, readExcel method contains a parameter "Index".
+- Index is nothing but row number of the starting URL in the excel document.
+- Default value of the index is '1'. No need to change it unless there is a script failure. 
+- Incase the script is failed after generating 150 URLs, then we need to change the index value as 151 and restart the script. 
+- This updated value skip the completed URLs and start with the 151st URL.
+
+## Parameter - FileSeq:
+- In "URLtoScreenshot.Java" file, readExcel method contains a parameter "fileSeq".
+- Default value is 0.
+- When we need to run 2 excel files parallelly and each file contains 1000 URLs, then 'fileSeq' value for first test should be '0'. For the 2nd test the value should be '1001'.
+- When we need to run 4 excel files parallelly/sequencially, then we should give '0' to first test, 1001 to 2nd test, 2001 to 1st test 2nd run and 3001 to 2nd test 2nd run. 
--- src/test/java/demo/deque/urlToScreenshots/URLtoScreenshot.java
+++ src/test/java/demo/deque/urlToScreenshots/URLtoScreenshot.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.imageio.ImageIO;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -16,128 +19,99 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 
 import com.assertthat.selenium_shutterbug.core.Shutterbug;
 import com.assertthat.selenium_shutterbug.utils.web.ScrollStrategy;
+import com.google.common.net.InternetDomainName;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class URLtoScreenshot {
 
-	public static WebDriver driver;
+	//public WebDriver driver;
 
-	@SuppressWarnings({ "resource"})
-	public void readExcel(String filePath, String fileName, String sheetName) throws IOException {
-		
-		int width = 1920;
-        int height = 1080;
-		
-		//Storing failed urls in a separate file for tracking
-		PrintWriter failedList = new PrintWriter("failedURLs.txt");		
-		//WebDriverManager.firefoxdriver().setup();
-		//driver = new FirefoxDriver();
-		WebDriverManager.chromedriver().setup();
-		
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless");
-		//WebDriver driver = new ChromeDriver(options);
-		
-		driver = new ChromeDriver(options);		
-		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);		
-		
-		
-        //Remove the window from fullscreen (optional), if it s in fullscreen the outerHeight is not accurate
-        driver.manage().window().setSize(new Dimension(800,800));
-        JavascriptExecutor js= (JavascriptExecutor)driver;
-
-        String windowSize = js.executeScript("return (window.outerWidth - window.innerWidth + "+width+") + ',' + (window.outerHeight - window.innerHeight + "+height+"); ").toString();
-
-        //Get the values
-        width = Integer.parseInt(windowSize.split(",")[0]);
-        height = Integer.parseInt(windowSize.split(",")[1]);
-        System.out.println("Width is :"+width+ "and height is :"+height);
-
-        //Set the window
-        driver.manage().window().setSize(new Dimension(width, height));       
-        
-        //Dimension dimension = new Dimension(1920, 1080);
-        //driver.manage().window().setSize(dimension);
-
-		// Create an object of File class to open xlsx file
-		File file = new File(filePath + "\\" + fileName);
-
-		// Create an object of FileInputStream class to read excel file
-		FileInputStream inputStream = new FileInputStream(file);
-
-		Workbook myWorkbook = null;
-
-		myWorkbook = new XSSFWorkbook(inputStream);
-
-		// Read sheet inside the workbook by its name
-		Sheet mySheet = myWorkbook.getSheet(sheetName);
-
-		// Find number of rows in excel file - this will be passed in the below for loop
-		int rowCount = mySheet.getLastRowNum() - mySheet.getFirstRowNum();
-
-		String targetFilePath = System.getProperty("user.dir") + "\\target\\screenshots\\";
-
-		// Create a loop over all the rows of excel file to read it
-		//for (int i = 1; i < rowCount + 1; i++) {
-		for (int i = 2; i < rowCount + 1; i++) {
-			Row row = mySheet.getRow(i);
-			String url = row.getCell(0).getStringCellValue();//1st column of the excel contains all the URLs
-			//String tcName = (row.getCell(0).getStringCellValue()).replaceAll("\\s", "");
-			if(!(url.contains("http://") || url.contains("https://"))) {
-				url = "https://"+url;			
+		@SuppressWarnings({ "resource" })
+		public void readExcel(String filePath, String fileName, String sheetName, int fileSeq, String failList, int index)
+				throws IOException {
+			int width = 1920;
+			int height = 1080;
+			PrintWriter failedList = new PrintWriter(failList);
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless");
+			WebDriver driver = new ChromeDriver(options);
+			driver.manage().window().setSize(new Dimension(800, 800));
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			String windowSize = js.executeScript("return (window.outerWidth - window.innerWidth + " + width
+					+ ") + ',' + (window.outerHeight - window.innerHeight + " + height + "); ").toString();
+			width = Integer.parseInt(windowSize.split(",")[0]);
+			height = Integer.parseInt(windowSize.split(",")[1]);
+			System.out.println("Width is :" + width + "and height is :" + height);
+			driver.manage().window().setSize(new Dimension(width, height));
+			File file = new File(filePath + "\\" + fileName);
+			FileInputStream inputStream = new FileInputStream(file);
+			Workbook myWorkbook = null;
+			myWorkbook = new XSSFWorkbook(inputStream);
+			Sheet mySheet = myWorkbook.getSheet(sheetName);
+			int rowCount = mySheet.getLastRowNum() - mySheet.getFirstRowNum();
+			String targetFilePath = System.getProperty("user.dir") + "\\target\\screenshots\\";
+			String skippedFilePath = System.getProperty("user.dir") + "\\target\\skipped_screenshots\\";
+			//index is nothing but the row number in the excel file. Sometimes screenshots are generated for only some rows. So
+			//this index number is useful while rerunning the same file by excluding completed URLs.		
+			for (int i = index; i < rowCount + 1; i++) {
+				Row row = mySheet.getRow(i);
+				String url = row.getCell(0).getStringCellValue();// 1st column of the excel contains all the URLs
+				if (!(url.contains("http://") || url.contains("https://"))) {
+					url = "https://" + url;
+					}			
+				try {
+					driver.get(url);// Selenium command to launch URL.
+					Thread.sleep(20000);
+					BufferedImage image = Shutterbug.shootPage(driver, ScrollStrategy.WHOLE_PAGE, 5).getImage();
+					int seqNo = fileSeq + i;				
+					int imageHeight = image.getHeight();
+					//Skipping all the screenshots with height more than 5000px.
+					String domainName = extractDomain(url);
+					if(imageHeight > 5000) {
+						System.out.println(seqNo + " - Skipping it. Height of this screenshot is more thatn 5000 px. Height is : "+imageHeight);
+						ImageIO.write(image, "png", new File(skippedFilePath + seqNo + "-" + imageHeight +"-"+domainName+".png"));
+					}
+					else {
+						ImageIO.write(image, "png", new File(targetFilePath + seqNo+"-"+domainName+ ".png"));
+						System.out.println(seqNo+" - Successfully genereated screenshot for the URL:" + url);
+					}				
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.out.println("Page 404 issue is observed with the URL : " + url);
+					driver.navigate().refresh();
+					failedList.println(i + "-" + url);
+				}
 			}
-			System.out.println("Updated URL is :" + url);
-			try {
-				driver.get(url);//Selenium command to launch URL.
-				Thread.sleep(30000);
-				BufferedImage image = Shutterbug.shootPage(driver, ScrollStrategy.WHOLE_PAGE, 5).getImage();				
-				//BufferedImage image = Shutterbug.shootPage(driver, ScrollStrategy.VIEWPORT_ONLY).getImage();
-				int seqNo = 0 + i;
-				ImageIO.write(image, "png", new File(targetFilePath + seqNo + ".png"));				
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println("Page 404 issue is observed with the URL : " + url);
-				failedList.println(i + "-" + url);			
-			}
+			failedList.close();
+			driver.close();
+
 		}
-		failedList.close();
+		
+		public String extractDomain(String url) throws MalformedURLException {
+		     URL myUrl = new URL(url);
+		         String host = myUrl.getHost();
+		         InternetDomainName name1 = InternetDomainName.from(host).topPrivateDomain();
+		         String domain = name1.toString();
+		         return domain;
+		}
+
+		@Test
+		public void script1() throws IOException {		
+			String filePath = System.getProperty("user.dir") + "\\src\\resources";
+			readExcel(filePath, "URLsDocument.xlsx", "urls", 0, "failedURLs_1.txt", 1);		
+		}	
+		
+		//Uncomment this test when you want to run a couple of URL files parallelly. Run testng.xml to kick start the parallel execution.
+		//@Test
+		public void script2() throws IOException {		
+			String filePath = System.getProperty("user.dir") + "\\src\\resources";
+			readExcel(filePath, "122021URLs_1001_to_2000.xlsx", "April06-2022", 1000, "failedURLs_2.txt", 816);		
+		}
 
 	}
-
-	// Main function is calling readExcel function to read data from excel file
-	public static void main(String... strings) throws IOException {
-		// Create an object of the excelFile class
-		URLtoScreenshot objExcelFile = new URLtoScreenshot();
-		// Prepare the path of the excel file
-		String filePath = System.getProperty("user.dir") + "\\src\\resources";
-		// Call read file method of the class to read data
-		objExcelFile.readExcel(filePath, "apex_batch7_part2.xlsx", "Sheet1");
-		driver.close();
-	}
-
-}
-
-/*
- * AShot implementation - take screenshot of the entire page Screenshot
- * 
- * import ru.yandex.qatools.ashot.AShot; 
- * import ru.yandex.qatools.ashot.Screenshot;
- * import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
- * 
- * screenshot=new
- * AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).
- * takeScreenshot(driver); try { ImageIO.write(screenshot.getImage(),"PNG",new
- * File(targetFilePath + i + ".png")); } catch (IOException e) { // TODO
- * Auto-generated catch block e.printStackTrace(); }
- * 
- * //driver.quit();
- * 
-				
-//WebDriverWait wait = new WebDriverWait(driver, 120);
-//wait.until(WebDriver -> ((JavascriptExecutor) driver).executeScript("return document.readyState").toString().equals("complete"));
-//js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
- */
